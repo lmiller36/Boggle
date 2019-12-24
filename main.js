089b1aeaccf969b83e8874215ac116082c4bfad8
Lorne Miller <lmiller36@wisc.edu>
2019-12-23 21:42:12 -0600

    Rotation feature

--- main.js
+++ main.js
@@ -23,6 +23,7 @@
 
 	document.numberOfPlayers = 1;
 	document.submittedWords = [];
+	document.currRotation = 0;
 
 	function isWord(word){
 		var isEnglishWord = document.wordlist.indexOf(word) != -1;
@@ -163,52 +164,100 @@
 
 		var shuffled = shuffledBoard();
 
-		finished = 1;
-		arr = shuffled;
-		if(finished){
-			document.board = arr;
-			for(var i = 0; i < 5; i++){
-				for(var j = 0; j < 5; j++){
-					var divContainer = document.createElement("div");
-					divContainer.id = "row_"+i+"_column_"+j;
-					divContainer.className = "grid-item";
-					divContainer.innerText = arr[i][j];
-					document.getElementById("grid-container").appendChild(divContainer);
+				// arr1.push(document.board[j][4 - i]);
+				// arr2.push(document.board[4 - i][4-j]);
+				// arr3.push(document.board[4 - j][i]);
+
+				finished = 1;
+				arr = shuffled;
+				if(finished){
+					document.board = arr;
+					for(var i = 0; i < 5; i++){
+						for(var j = 0; j < 5; j++){
+							var divContainer = document.createElement("div");
+							divContainer.id = "row_"+i+"_column_"+j+"_0";
+							divContainer.className = "grid-item";
+							divContainer.innerText = arr[i][j];
+
+							var divContainer1 = document.createElement("div");
+							divContainer1.id = "row_"+i+"_column_"+j+"_1";
+							divContainer1.className = "grid-item";
+							divContainer1.innerText = arr[j][4 - i];
+
+							var divContainer2 = document.createElement("div");
+							divContainer2.id = "row_"+i+"_column_"+j+"_2";
+							divContainer2.className = "grid-item";
+							divContainer2.innerText = arr[4 - i][4 - j];
+
+							var divContainer3 = document.createElement("div");
+							divContainer3.id = "row_"+i+"_column_"+j+"_3";
+							divContainer3.className = "grid-item";
+							divContainer3.innerText = arr[4 - j][i];
+
+							document.getElementById("board-0").appendChild(divContainer);
+							document.getElementById("board-1").appendChild(divContainer1);
+							document.getElementById("board-2").appendChild(divContainer2);
+							document.getElementById("board-3").appendChild(divContainer3);
+
+						}
+					}
+
+					document.getElementById("setup").style.display = "none";
+					document.getElementById("game").style.display = "";
+				}
+
+			}
+
+			function highlightBoard(obj){
+				let word = obj.value.toUpperCase();
+				let lettersToHighlight = wordOnBoard(word);
+				if(lettersToHighlight){
+					removeHighlightingFromAll();
+
+					lettersToHighlight.forEach((coords)=>{
+						highlightLetter(coords[0],coords[1]);
+					})
+
 				}
 			}
 
-			document.getElementById("setup").style.display = "none";
-			document.getElementById("game").style.display = "";
-		}
-
-	}
-
-	function highlightBoard(obj){
-		let word = obj.value.toUpperCase();
-		let lettersToHighlight = wordOnBoard(word);
-		if(lettersToHighlight){
-			removeHighlightingFromAll();
-
-			lettersToHighlight.forEach((coords)=>{
-				highlightLetter(coords[0],coords[1]);
-			})
-			
-		}
-	}
-
-	function removeHighlightingFromAll(){
-		for(var i = 0;i<5;i++){
-			for(var j = 0;j<5;j++){
-				var id = "row_"+i+"_column_"+j;
-				var div = document.getElementById(id);
-				div.style.border = "1px solid rgba(0, 0, 0, 0.8)";
-				div.style.background = "rgba(255, 255, 255, 0.8)";
+			function removeHighlightingFromAll(){
+				for(var rot = 0; rot < 4;rot++){
+					for(var i = 0;i<5;i++){
+						for(var j = 0;j<5;j++){
+							var id = "row_"+i+"_column_"+j+"_"+rot;
+							var div = document.getElementById(id);
+							div.style.border = "1px solid rgba(0, 0, 0, 0.8)";
+							div.style.background = "rgba(255, 255, 255, 0.8)";
+						}
+					}
+				}
 			}
-		}
-	}
-	function highlightLetter(row,col){
-		var id = "row_"+row+"_column_"+col;
-		var div = document.getElementById(id);
-		div.style.border = "white 1px solid";
-		div.style.background = "white";
-	}
+			function highlightLetter(row,col){
+				var id = "row_"+row+"_column_"+col+"_"+document.currRotation;
+				var div = document.getElementById(id);
+				div.style.border = "white 1px solid";
+				div.style.background = "white";
+			}
+
+			function rotateBoard(direction){
+				console.log(direction);
+				//hide current rotation
+				var currRotation = "board-"+document.currRotation;
+				console.log(currRotation);
+				document.getElementById(currRotation).style.display = "none";
+				console.log(document.getElementById(currRotation));
+				//advance
+				currRotation = (document.currRotation + direction) % 4;
+				if(currRotation == -1) currRotation = 3;
+				console.log(currRotation);
+
+				//show next rotation
+				var nextRotation = "board-"+currRotation;
+				document.getElementById(nextRotation).style.display = "grid";
+				console.log(nextRotation);
+				console.log(document.getElementById(nextRotation));
+
+				//save
+				document.currRotation = currRotation;
+			}
